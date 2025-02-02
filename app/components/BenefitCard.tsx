005130d1e156eb2337f2fc1d7a6a398b93559de6
Tim <poberezhny003@gmail.com>
2025-02-02 16:47:00 +0100

    Dark theme support + Lint

--- app/components/BenefitCard.tsx
+++ app/components/BenefitCard.tsx
@@ -16,14 +16,14 @@ const BenefitCard: React.FC<BenefitCardProps> = ({
 
   return (
     <div
-      className="relative aspect-square md:aspect-auto bg-cover bg-center rounded-md overflow-hidden w-[323px] h-[358px] md:h-[498px] bg-gray-500"
+      className="relative aspect-square md:aspect-auto bg-cover bg-center rounded-md overflow-hidden w-[323px] h-[358px] md:h-[498px] bg-gray-500 mb-8"
       style={{ backgroundImage: `url(${imageUrl})` }}
     >
       <div className={`absolute ${positionClasses} p-4`}>
         {blurTextBg ? (
-          <div className="bg-white bg-opacity-70 background-blur-sm p-2 rounded">
-            <h2 className="font-semibold text-lg">{title}</h2>
-            <p className="text-sm mt-3">{description}</p>
+          <div className="bg-white bg-opacity-70 background-blur-sm p-3 rounded dark:invert">
+            <h2 className="font-semibold text-lg dark:invert">{title}</h2>
+            <p className="text-sm mt-3 dark:invert">{description}</p>
           </div>
         ) : (
           <>
