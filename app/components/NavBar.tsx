0d4e8b09e5ac84cc7ed3d3f672b36e9a8f59a0fe
Tim <poberezhny003@gmail.com>
2025-02-02 18:37:37 +0100

    Implementing non functional ContactModal

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -4,10 +4,14 @@ import Image from "next/image";
 import Link from "next/link";
 import { ModeToggle } from "./ModeToggle";
 import { useTheme } from "next-themes";
+import { Button } from "@/components/ui/button";
+import ContactModal from "./ContactModal";
+import { useState } from "react";
 
 export default function NavBar() {
   const { theme, systemTheme } = useTheme();
   const currentTheme = theme === "system" ? systemTheme : theme;
+  const [contactOpen, setContactOpen] = useState(false);
 
   if (!currentTheme) return null;
 
@@ -17,9 +21,7 @@ export default function NavBar() {
       : "/assets/whiteThemeLogo.svg";
 
   return (
-    <nav
-      className="fixed top-0 w-full z-50 backdrop-blur-md bg-white/70 dark:bg-gray-900/70  transition-all"
-    >
+    <nav className="fixed top-0 w-full z-50 backdrop-blur-md bg-white/70 dark:bg-gray-900/70  transition-all">
       <div className="flex items-center justify-between max-w-6xl mx-auto px-4 py-4">
         {/* Logo */}
         <Link href="/">
@@ -34,41 +36,36 @@ export default function NavBar() {
 
         {/* Navigation Links */}
         <div className="hidden md:flex gap-7">
-          <Link
-            href="/#furniture"
-            className="nav-link" scroll={true}
-          >
+          <Link href="/#furniture" className="nav-link" scroll={true}>
             Furniture
           </Link>
-          <Link
-            href="/#benefits"
-            className="nav-link"
-          >
+          <Link href="/#benefits" className="nav-link">
             Benefits
           </Link>
-          <Link
-            href="/howItWorks"
-            className="nav-link"
-          >
+          <Link href="/howItWorks" className="nav-link">
             How it Works
           </Link>
-          <Link
-            href="/prices"
-            className="nav-link"
-          >
+          <Link href="/prices" className="nav-link">
             Prices
           </Link>
-          <Link
-            href="/faq"
-            className="nav-link"
-          >
+          <Link href="/faq" className="nav-link">
             FAQ
           </Link>
         </div>
 
-        {/* Mode Toggle */}
-        <ModeToggle />
+        {/* Contact Button */}
+        <div className="flex items-center gap-4">
+          <Button
+            onClick={() => setContactOpen(true)}
+            variant="default"
+            className="bg-primary text-white"
+          >
+            Contact Us
+          </Button>
+          <ModeToggle />
+        </div>
       </div>
+      {contactOpen && <ContactModal onClose={() => setContactOpen(false)} />}
     </nav>
   );
 }
