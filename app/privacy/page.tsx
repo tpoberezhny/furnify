b155dcd09bb8685442face6fa4eb1c91261d3f93
Tim <poberezhny003@gmail.com>
2025-02-08 19:19:25 +0100

    Invert color text for privacy&terms

--- app/privacy/page.tsx
+++ app/privacy/page.tsx
@@ -1,7 +1,7 @@
-import { groq } from 'next-sanity';
-import { PortableText } from '@portabletext/react';
-import { client } from '../lib/sanity';
-import type { PortableTextBlock } from '@portabletext/react';
+import { groq } from "next-sanity";
+import { PortableText } from "@portabletext/react";
+import { client } from "../lib/sanity";
+import type { PortableTextBlock } from "@portabletext/react";
 
 export const revalidate = 60;
 
@@ -17,13 +17,13 @@ export default async function PrivacyPage() {
       content
     }
   `;
-  
+
   const privacy: PrivacyData = await client.fetch(query);
 
   return (
     <main className="container mx-auto px-4 py-8 max-w-6xl">
-      <article className="prose lg:prose-xl mt-16">
-        <h1 className=''>{privacy.title}</h1>
+      <article className="prose lg:prose-xl dark:invert mt-16">
+        <h1 className="">{privacy.title}</h1>
         <PortableText value={privacy.content} />
       </article>
     </main>
